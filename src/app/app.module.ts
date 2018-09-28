b3c19c5237170f2bb1d02e22b509d48b8124dc03
DESKTOP-V9GGFFC\Pathum <plakshan94617@gmail.com>
2018-09-28 15:02:42 +0530

    FirstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes : Routes = [
+  { path: 'crisis-center', component: HomeComponent }
+  {
+    path: 'heroes',
+    component: HomeComponent,
+    data: { title: 'Heroes List' }
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +25,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
